0a5ee89b93833295e8b70bc81e9178d5ed8ef8d0
JC Manzo <jnmanzo@gmail.com>
2015-08-12 11:15:03 -0700

    Changed Artists fragment to use RecyclerView and its adapter

--- app/src/main/java/com/freneticlabs/cleff/fragments/ArtistsFragment.java
+++ app/src/main/java/com/freneticlabs/cleff/fragments/ArtistsFragment.java
@@ -1,71 +1,64 @@
 package com.freneticlabs.cleff.fragments;
 
-
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.freneticlabs.cleff.CleffApp;
 import com.freneticlabs.cleff.R;
 import com.freneticlabs.cleff.models.Artist;
 import com.freneticlabs.cleff.models.MusicLibrary;
 import com.freneticlabs.cleff.views.adapters.ArtistAdapter;
-
 import java.util.ArrayList;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ArtistsFragment extends Fragment {
 
+  public ArtistsFragment() {
+    // Required empty public constructor
+  }
 
-    public ArtistsFragment() {
-        // Required empty public constructor
-    }
+  @Bind(R.id.artists_view) RecyclerView mRecyclerView;
+  private ArtistAdapter mArtistsAdapter;
+  private ArrayList<Artist> mArtists;
+  private Context mContext;
+  private CleffApp mCleffApp;
 
-    @Bind(R.id.artists_grid_view)
-    GridView mGridView;
-    private ArtistAdapter mArtistsAdapter;
-    private ArrayList<Artist> mArtists;
-    private SharedPreferences mSettings;
-    private Context mContext;
-    private CleffApp mCleffApp;
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+    mContext = getActivity().getApplicationContext();
+    mCleffApp = (CleffApp) getActivity().getApplication();
+    mArtists = new ArrayList<Artist>(MusicLibrary.getInstance(mContext).getAllArtists());
+  }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        mContext = getActivity().getApplicationContext();
-        mCleffApp = (CleffApp)getActivity().getApplication();
-        mSettings = mCleffApp.getAppPreferences();
-        mArtists = new ArrayList<Artist>(MusicLibrary.getInstance(mContext).getAllArtists());
-    }
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    super.onCreateView(inflater, container, savedInstanceState);
+    // Inflate the layout for this fragment
+    View rootView = inflater.inflate(R.layout.fragment_artist_grid, container, false);
+    ButterKnife.bind(this, rootView);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_artist_list, container, false);
-        ButterKnife.bind(this, rootView);
+    setUpRecyclerViewAndAdapter();
+    return rootView;
+  }
 
-        mArtistsAdapter = new ArtistAdapter(mContext, mArtists);
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    ButterKnife.unbind(this);
+  }
 
-        mGridView.setAdapter(mArtistsAdapter);
-        return rootView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        ButterKnife.unbind(this);
-    }
+  private void setUpRecyclerViewAndAdapter() {
+    mArtistsAdapter = new ArtistAdapter(mContext, mRecyclerView, mArtists);
+    mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+    mRecyclerView.setAdapter(mArtistsAdapter);
+  }
 }
--- app/src/main/java/com/freneticlabs/cleff/views/adapters/ArtistAdapter.java
+++ app/src/main/java/com/freneticlabs/cleff/views/adapters/ArtistAdapter.java
@@ -1,87 +1,54 @@
 package com.freneticlabs.cleff.views.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.freneticlabs.cleff.CleffApp;
 import com.freneticlabs.cleff.R;
 import com.freneticlabs.cleff.models.Artist;
-
 import java.util.ArrayList;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * Created by jcmanzo on 7/11/15.
  */
-public class ArtistAdapter extends ArrayAdapter<Artist> {
-    private ArrayList<Artist> mArtistList;
-    private final Context mContext;
-    private LayoutInflater mArtistsInflater;
-    private String mSongCount;
-    private CleffApp mCleffApp;
+public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ViewHolder> {
+  private ArrayList<Artist> mArtistList;
+  private final Context mContext;
+  private LayoutInflater mArtistsInflater;
+  private String mSongCount;
+  private CleffApp mCleffApp;
 
-    public ArtistAdapter(Context context, ArrayList<Artist> artists) {
-        super(context, 0, artists);
-        mContext = context;
-        mArtistList = artists;
-        mCleffApp = (CleffApp) mContext.getApplicationContext();
-        mArtistsInflater = LayoutInflater.from(context);
+  public ArtistAdapter(Context context, RecyclerView recyclerView, ArrayList<Artist> artists) {
+    mContext = context;
+    mArtistList = artists;
+    mCleffApp = (CleffApp) mContext.getApplicationContext();
+  }
+
+  public static class ViewHolder extends RecyclerView.ViewHolder {
+    @Bind(R.id.grid_artist_title) TextView artistName;
+
+    public ViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
     }
+  }
 
-    static class ViewHolder {
-        @Bind(R.id.grid_artist_title)
-        TextView title;
+  @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_artists_grid, parent, false);
+    return new ViewHolder(view);
+  }
 
-        public ViewHolder(View view) {
-            ButterKnife.bind(this, view);
-        }
-    }
+  @Override public int getItemCount() {
+    return mArtistList.size();
+  }
 
-    @Override
-    public int getCount() {
-        return mArtistList.size();
-    }
+  @Override public void onBindViewHolder(ViewHolder holder, int position) {
+    holder.artistName.setText(mArtistList.get(position).getArtistName());
 
-    @Override
-    public Artist getItem(int position) {
-        return mArtistList.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(final int position, View view, ViewGroup parent) {
-        ViewHolder holder;
-        if(view == null) {
-            //map to song layout
-            view = mArtistsInflater.inflate(R.layout.item_artists_grid, parent, false);
-
-            // Set up up the ViewHolder
-            holder = new ViewHolder(view);
-            holder.title = (TextView)view.findViewById(R.id.grid_artist_title);
-
-            view.setTag(holder);
-
-        } else {
-            // View already exists
-            holder = (ViewHolder)view.getTag();
-        }
-
-        //get song using position
-        final Artist Artist = mArtistList.get(position);
-        //get title and artist strings
-        holder.title.setText(Artist.getArtistName());
-
-
-        return view;
-    }
+  }
 }
